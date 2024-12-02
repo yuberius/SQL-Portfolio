6acd63d66bc46e247897b02bdc3e3e024a01e688
yuberius <48984135+yuberius@users.noreply.github.com>
2024-12-02 22:25:49 +0100

    Add files via upload

---
+++ Age and Sex data pivoted by RegistrationGroup.sql
@@ -0,0 +1,39 @@
+select [Registration Group ID]
+    ,sum([Girls]) 'Girls'
+    ,sum ([Boys]) 'Boys'
+    ,sum([Women]) 'Women'
+    ,sum([Men]) 'Men'
+    ,sum([Elderly Women]) 'Elderly Women'
+    ,sum([Elderly Men]) 'Elderly Men'
+from(
+SELECT [Registration Group ID],
+    CASE
+        WHEN ([Age] < 18) and (Sex = 'Female') THEN 1
+        ELSE 0
+    END 'Girls',
+    CASE
+        WHEN ([Age] < 18) and (Sex = 'Male') THEN 1
+        ELSE 0
+    END 'Boys',
+    CASE
+        WHEN ([Age] > 17) and ([Age] < 60) and (Sex = 'Female') THEN 1
+        ELSE 0
+    END 'Women',
+    CASE
+        WHEN ([Age] > 17) and ([Age] < 60) and (Sex = 'Male') THEN 1
+        ELSE 0
+    END 'Men',
+    CASE
+        WHEN ([Age] > 59) and (Sex = 'Female') THEN 1
+        ELSE 0
+    END 'Elderly Women',
+    CASE
+        WHEN ([Age] > 59) and (Sex = 'Male') THEN 1
+        ELSE 0
+    END 'Elderly Men'
+  FROM [progresSSISdb].[dbo].[Ind]
+where [Registration Group ID] in (
+        'Reg group IDs'
+  )
+) as Temp
+group by [Registration Group ID]
